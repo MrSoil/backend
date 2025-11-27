feafe76e82372969f54420cff73effa7b193d117
Toprak Ceylan <toprakceylan01@gmail.com>
2025-11-27 20:07:12 +0300

    docker success v8

--- sugr_backend/migrations/0005_auto_20251126_1517.py
+++ sugr_backend/migrations/0005_auto_20251126_1517.py
@@ -10,14 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='firstname',
-        ),
-        migrations.RemoveField(
-            model_name='user',
-            name='lastname',
-        ),
         migrations.AlterField(
             model_name='user',
             name='first_name',
